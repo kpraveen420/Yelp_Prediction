35abac2e9d0e93bed616bcdaa72dd55639c14ebc
Praveen Kanamarlapudi <kpraveen420@gmail.com>
2015-12-01 19:58:57 -0800

    first commit

---
+++ prediction_algorithm.py
@@ -0,0 +1,55 @@
+__author__ = 'praveenk'
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+import data_importer
+
+
+def train():
+    yelp_data = data_importer.get_yelp_data()
+    test_data = np.array(yelp_data)
+    # print test_data
+    yelp_x = yelp_data['data']
+    yelp_y = yelp_data['target']
+    yelp_x_train = yelp_x[:len(yelp_x)*80/100]
+    yelp_y_train = yelp_y[:len(yelp_y)*80/100]
+
+    yelp_x_test = yelp_x[(len(yelp_x)*80/100)+1:]
+    yelp_y_test = yelp_y[(len(yelp_y)*80/100)+1:]
+
+    print yelp_x_test[:10]
+    print yelp_y_test[:10]
+
+    # Create linear regression object
+    regr = linear_model.LinearRegression()
+
+    # Train the model using the training sets
+    regr.fit(yelp_x_train, yelp_y_train)
+
+    # The coefficients
+    print('Coefficients: \n', regr.coef_)
+    # The mean square error
+    print("Residual sum of squares: %.2f"% np.mean((regr.predict(yelp_x_test) - yelp_y_test) ** 2))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score: %.2f' % regr.score(yelp_x_test, yelp_y_test))
+    # print( len(yelp_x))
+    # print len(yelp_x_test)
+    # print len(yelp_y_test)
+    # Plot outputs
+    print regr.predict([1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 2, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0])
+    return regr
+
+
+def predict( regr, business_id, features):
+    business_features = data_importer.get_bussiness_features_by_id(business_id)
+    print business_features
+    updated_features = data_importer.change_features(business_features, features)
+    print updated_features
+    print updated_features
+    print updated_features
+
+    return {'new_rating': regr.predict(updated_features)[0]};
+
+
+# train()
