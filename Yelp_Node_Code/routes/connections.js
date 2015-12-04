5ed63903bc53d254796175469e5523e0eaae77d7
Praveen Kanamarlapudi <kpraveen420@gmail.com>
2015-12-04 02:34:03 -0800

    Update connections.js

--- Yelp_Node_Code/routes/connections.js
+++ Yelp_Node_Code/routes/connections.js
@@ -3,8 +3,8 @@ function dbConnection()
 {
 	var sqlDB = database.createConnection({
 										host     : '54.218.47.42',
-										user     : 'root',
-										password : 'root',
+										user     : 'user',
+										password : 'password',
 										database: 'yelp_v2'
 									  });
 	sqlDB.connect();
